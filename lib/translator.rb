9b8be545b830506b1cb311080c8aa75cc53277a4
Matthew C <m23.cullen@gmail.com>
2019-08-12 16:29:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,12 @@
 # require modules here
+#get meaning is {japanese emote => meaning}
+#get_emotion is {eng emote => japanese emote}
 require "yaml"
 
 def load_library
   library = YAML.load_file('emoticons.yml')
-  sorted = {"get_meaning" => {}, "get_emotion" => {}}
-  library.each {|emotion,eng0_jap1|
+  sorted = {"get_meaning" => {}, "get_emotion" => {}} 
+  library.each {|emotion,array| #array is english at i[0] and japanese at i[1]
     sorted["get_meaning"]
   }
 end
