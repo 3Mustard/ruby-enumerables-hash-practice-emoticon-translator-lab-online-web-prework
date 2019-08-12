5f2e811b87ddd136ce4e322ffcc43d65a19345cb
Matthew C <m23.cullen@gmail.com>
2019-08-12 16:26:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,10 @@ require "yaml"
 
 def load_library
   library = YAML.load_file('emoticons.yml')
-  
+  sorted = {"get_meaning" => {}, "get_emotion" => {}}
+  library.each {|emotion,eng0_jap1|
+    sorted["get_meaning"]
+  }
 end
 
 def get_japanese_emoticon
