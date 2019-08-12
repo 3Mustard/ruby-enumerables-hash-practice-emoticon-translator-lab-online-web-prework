36d9c7bc8d2c7911a6d13ff7070a0bd40083820d
Matthew C <m23.cullen@gmail.com>
2019-08-12 19:28:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 def get_japanese_emoticon(file_path,emoticon_english)
   emoticons = load_library(file_path)
   if emoticons[get_emoticon].include?(emoticon_english)
-    
+    emoticons[get_emoticon][]
 end
 
 def get_english_meaning
